c1b2215f0f5bac8c37c8749cbc09c6785e1674e8
pearthip31-netizen <pearthip31@gmail.com>
2026-01-12 15:44:55 +0700

    first commit

---
+++ 68026495/Work1.py
@@ -0,0 +1,19 @@
+som = 10        
+apple = 15      
+grape = 10      
+som -= 4        
+apple += 2
+grape += 2      
+grape_som = 2
+apple -= 5
+som += 5
+som -= 2
+grape_apple = 2  
+grape -= 2
+print("หลังเหตุการณ์ทั้งหมด:")
+print("ส้มมีส้ม =", som, "ลูก")
+print("แอปเปิ้ลมีแอปเปิ้ล =", apple, "ลูก")
+print("องุ่นมีองุ่น =", grape, "พวง")
+print("องุ่นมีส้มที่ได้รับ =", grape_som, "ลูก")
+print("องุ่นมีแอปเปิ้ลที่ขโมยได้ =", grape_apple, "ลูก")
+ 
